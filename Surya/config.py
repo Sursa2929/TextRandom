f668d8e72749c171c860693d72fea70d83ed8767
Surya Bishnoi love ❤️  B <166400821+Sursa2929@users.noreply.github.com>
2024-05-19 20:25:42 +0530

    Update config.py

--- Surya/config.py
+++ Surya/config.py
@@ -24,3 +24,29 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 ADMINS.append(OWNER)
 
+api_id = 28136850
+api_hash = "ef27e112f853d3779d38204f7800ba24"
+bot_token = "7056596416:AAFp3ZH8ugPoHlNKysl7jwlO8Kx08wULSXM"
+auth_users = "841021123"
+sudo_users = [int(num) for num in auth_users.split(",")]
+osowner_users = "841021123"
+owner_users = [int(num) for num in osowner_users.split(",")]
+
+
+import os
+
+
+
+api_id = 3748059
+
+api_hash = os.environ.get("API_HASH", "f8c9df448f3ba20a900bc2ffc8dae9d5")
+
+bot_token = os.environ.get("BOT_TOKEN")
+
+auth_users = os.environ.get("AUTH_USERS", "5591734243,1369808729")
+
+sudo_users = [int(num) for num in auth_users.split(",")]
+
+osowner_users = os.environ.get("OWNER_USERS", "5591734243,1369808729")
+
+owner_users = [int(num) for num in osowner_users.split(",")]
