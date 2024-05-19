bf4bce7a6f688d08e3bfb5f2e5ad078b747d1587
Surya Bishnoi love ❤️  B <166400821+Sursa2929@users.noreply.github.com>
2024-05-19 20:42:23 +0530

    Update config.py

--- Surya/config.py
+++ Surya/config.py
@@ -6,13 +6,27 @@ import os
 
 #Bot Created by Surya Bishnoi love ❤️ B 
 
-API_ID = os.environ.get("API_ID")
+API_ID = int(os.environ.get("API_ID"))
 
 API_HASH = os.environ.get("API_HASH")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
+auth_users = int(os.environ.get("AUTH_USERS"))
+
+sudo_users = [int(num) for num in auth_users.split(",")]
+
+osowner_users = int(os.environ.get("OWNER_USERS"))
+
+owner_users = [int(num) for num in osowner_users.split(",")]
+
+
+
+
+
+
+
+```PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 841021123))
 
@@ -30,23 +44,4 @@ bot_token = "7056596416:AAFp3ZH8ugPoHlNKysl7jwlO8Kx08wULSXM"
 auth_users = "841021123"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "841021123"
-owner_users = [int(num) for num in osowner_users.split(",")]
-
-
-import os
-
-
-
-api_id = 3748059
-
-api_hash = os.environ.get("API_HASH", "f8c9df448f3ba20a900bc2ffc8dae9d5")
-
-bot_token = os.environ.get("BOT_TOKEN")
-
-auth_users = os.environ.get("AUTH_USERS", "5591734243,1369808729")
-
-sudo_users = [int(num) for num in auth_users.split(",")]
-
-osowner_users = os.environ.get("OWNER_USERS", "5591734243,1369808729")
-
-owner_users = [int(num) for num in osowner_users.split(",")]
+owner_users = [int(num) for num in osowner_users.split(",")]```
